374cfb34aae7a27d5f11186b0bc6c152981a83bd
이재원 <jenyglee_30@naver.com>
2023-07-27 15:36:14 +0900

    react-daum-postcode 라이브러리로 주소검색 구현

--- app/templates/DetailTemplate04/index.tsx
+++ app/templates/DetailTemplate04/index.tsx
@@ -1,6 +1,24 @@
 import styles from "./index.module.scss";
+import DaumPostcode from "react-daum-postcode";
+import { useEffect, useState } from "react";
 
 function DetailTemplate04() {
-    return <div>주소 검색 API 구현해보장</div>;
+    const [address, setAddress] = useState("");
+    const [zonecode, setZonecode] = useState("");
+    const handleComplete = (data: any) => {
+        setAddress(data.address);
+        setZonecode(data.zonecode);
+    };
+
+    useEffect(() => {
+        console.log("address : ", address);
+        console.log("zonecode : ", zonecode);
+    }, [address, zonecode]);
+
+    return (
+        <div>
+            <DaumPostcode onComplete={handleComplete} />
+        </div>
+    );
 }
 export default DetailTemplate04;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,157 +1,26 @@
 import { AppProps } from "next/app";
-import { createGlobalStyle } from "styled-components";
 import Layout from "../app/containers/Layout";
 // common css
 import "styles/global.scss";
-
+import Script from "next/script";
+declare global {
+    // Kakao 함수를 전역에서 사용할 수 있도록 선언
+    interface Window {
+        Kakao: any;
+    }
+}
 function App({ Component, pageProps }: AppProps) {
+    function kakaoInit() {
+        // 페이지가 로드되면 실행
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        console.log(window.Kakao.isInitialized());
+    }
     return (
         <Layout>
             <Component {...pageProps} />
+            <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit}></Script>
         </Layout>
     );
 }
-// const GlobalStyle = createGlobalStyle`
-//     html,
-//     body,
-//     div,
-//     span,
-//     applet,
-//     object,
-//     iframe,
-//     h1,
-//     h2,
-//     h3,
-//     h4,
-//     h5,
-//     h6,
-//     p,
-//     blockquote,
-//     pre,
-//     a,
-//     abbr,
-//     acronym,
-//     address,
-//     big,
-//     cite,
-//     code,
-//     del,
-//     dfn,
-//     em,
-//     img,
-//     ins,
-//     kbd,
-//     q,
-//     s,
-//     samp,
-//     small,
-//     strike,
-//     strong,
-//     sub,
-//     sup,
-//     tt,
-//     var,
-//     b,
-//     u,
-//     i,
-//     center,
-//     dl,
-//     dt,
-//     dd,
-//     ol,
-//     ul,
-//     li,
-//     fieldset,
-//     form,
-//     label,
-//     legend,
-//     table,
-//     caption,
-//     tbody,
-//     tfoot,
-//     thead,
-//     tr,
-//     th,
-//     td,
-//     article,
-//     aside,
-//     canvas,
-//     details,
-//     embed,
-//     figure,
-//     figcaption,
-//     footer,
-//     header,
-//     hgroup,
-//     menu,
-//     nav,
-//     output,
-//     ruby,
-//     section,
-//     summary,
-//     time,
-//     mark,
-//     audio,
-//     video {
-//         margin: 0;
-//         padding: 0;
-//         border: 0;
-//         font-size: 100%;
-//         font: inherit;
-//         vertical-align: baseline;
-//     }
-//     /* HTML5 display-role reset for older browsers */
-//     article,
-//     aside,
-//     details,
-//     figcaption,
-//     figure,
-//     footer,
-//     header,
-//     hgroup,
-//     menu,
-//     nav,
-//     section {
-//         display: block;
-//     }
-//     body {
-//         line-height: 1;
-//     }
-//     ol,
-//     ul {
-//         list-style: none;
-//     }
-//     blockquote,
-//     q {
-//         quotes: none;
-//     }
-//     blockquote:before,
-//     blockquote:after,
-//     q:before,
-//     q:after {
-//         content: "";
-//         content: none;
-//     }
-//     table {
-//         border-collapse: collapse;
-//         border-spacing: 0;
-//     }
-
-//     body {
-//         background-color: #F5F3FF;
-//     }
-
-//     .ck-rounded-corners .ck.ck-editor__top .ck-sticky-panel .ck-toolbar,
-//     .ck.ck-editor__top .ck-sticky-panel .ck-toolbar.ck-rounded-corners {
-//         border-top-left-radius: 8px !important;
-//         border-top-right-radius: 8px !important;
-//         background-color: #f7f7f7;
-//     }
-//     .ck-rounded-corners .ck.ck-editor__main > .ck-editor__editable,
-//     .ck.ck-editor__main > .ck-editor__editable.ck-rounded-corners {
-//         border-bottom-left-radius: 8px !important;
-//         border-bottom-right-radius: 8px !important;
-//     }
-// `;
 
 export default App;
